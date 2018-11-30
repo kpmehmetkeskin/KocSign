57a83d4edb0b0d468eb5ee9251530a9a067965ea
kpmehmetkeskin <mehmet.keskin@koctas.com.tr>
2018-11-30 11:29:25 +0300

    Mobilenumber adres bilgileri de ldaptan alınarak imza dinamik şekilde oluşturuldu.
    "C:\Users\current_user\AppData\Roaming\Microsoft\Signatures" altına dosyalar çıkartılır hale getirildi.

--- KocSign/KocSign/Form1.cs
+++ KocSign/KocSign/Form1.cs
@@ -19,18 +19,31 @@ namespace KocSign
         {
             using (var pc = new PrincipalContext(ContextType.Domain, "koctas.local"))
             {
+                String path = ".\\";
                 try
                 {
                     string name = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
                     var user = UserPrincipal.FindByIdentity(pc, IdentityType.SamAccountName, name);
+                    DirectoryEntry de = (user.GetUnderlyingObject() as DirectoryEntry);
 
+                    String displayName = user.DisplayName;
+                    String description = user.Description;
+                    String samAccountName = user.SamAccountName;
+                    String voiceTelephoneNumber = user.VoiceTelephoneNumber;
+                    String streetAddress = de.Properties["streetAddress"].Value.ToString();
+                    String mobile = de.Properties["extensionAttribute14"].Value.ToString();
+
+                    path = "C:\\Users\\"+samAccountName+"\\AppData\\Roaming\\Microsoft\\Signatures\\";
+                    
                     String filetext = File.ReadAllText("template.htm");
-                    filetext = filetext.Replace("\n", "").Replace("**name**", user.DisplayName).Replace("**position**", user.Description);
-                    File.WriteAllText("./me.htm", filetext, Encoding.UTF8);
+                    filetext = filetext.Replace("\n", "").Replace("**displayName**", displayName).Replace("**description**", description);
+                    filetext = filetext.Replace("**voiceTelephoneNumber**", voiceTelephoneNumber).Replace("**streetAddress**", streetAddress).Replace("**mobile**", mobile);
+
+                    File.WriteAllText(path + "me.htm", filetext, Encoding.UTF8);
 
                     String plainText = "\n\n\n" + user.DisplayName + "\n" + user.Description + "\n" + "KOÇTAŞ AŞ";
-                    File.WriteAllText("./me.rtf", plainText, Encoding.Unicode);
-                    File.WriteAllText("./me.txt", plainText, Encoding.Unicode);
+                    File.WriteAllText(path + "me.rtf", plainText, Encoding.Unicode);
+                    File.WriteAllText(path + "me.txt", plainText, Encoding.Unicode);
                 }
                 catch (Exception ex)
                 {
@@ -38,8 +51,8 @@ namespace KocSign
                     Environment.Exit(0);
                 }
 
-                MessageBox.Show("me.htm\nme.rtf\nme.txt\ndosyaları aynı dizine başarıyla oluşturuldu.","Başarılı");
-                Process.Start(System.IO.Path.GetDirectoryName(Application.ExecutablePath));
+                MessageBox.Show("me.htm\nme.rtf\nme.txt\ndosyaları "+path+" dizinine başarıyla oluşturuldu.","Başarılı");
+                Process.Start(path);
                 
                 Environment.Exit(0);
             }
