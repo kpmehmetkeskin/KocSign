173692b77de5bc8593be9ceb8dea308c1a941b26
kpmehmetkeskin <mehmet.keskin@koctas.com.tr>
2018-12-14 11:11:44 +0300

    Some fixes

--- KocSign/KocSign/Form1.cs
+++ KocSign/KocSign/Form1.cs
@@ -3,7 +3,9 @@ using System.Diagnostics;
 using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
 using System.IO;
+using System.Net.Mail;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace KocSign
@@ -31,29 +33,40 @@ namespace KocSign
                     String samAccountName = user.SamAccountName;
                     String voiceTelephoneNumber = user.VoiceTelephoneNumber;
                     String streetAddress = de.Properties["streetAddress"].Value.ToString();
-                    String mobile = de.Properties["extensionAttribute14"].Value.ToString();
+                    String mobile = de.Properties["mobile"].Value.ToString();
 
                     path = "C:\\Users\\"+samAccountName+"\\AppData\\Roaming\\Microsoft\\Signatures\\";
-                    
+
+                    System.IO.DirectoryInfo di = new DirectoryInfo(path);
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        file.Delete();
+                    }
+                    foreach (DirectoryInfo dir in di.GetDirectories())
+                    {
+                        dir.Delete(true);
+                    }
+
                     String filetext = File.ReadAllText("template.htm");
                     filetext = filetext.Replace("\n", "").Replace("**displayName**", displayName).Replace("**description**", description);
                     filetext = filetext.Replace("**voiceTelephoneNumber**", voiceTelephoneNumber).Replace("**streetAddress**", streetAddress).Replace("**mobile**", mobile);
 
-                    File.WriteAllText(path + "me.htm", filetext, Encoding.UTF8);
+                    File.WriteAllText(path + "Koçtaş İmza.htm", filetext, Encoding.UTF8);
 
                     String plainText = "\n\n\n" + user.DisplayName + "\n" + user.Description + "\n" + "KOÇTAŞ AŞ";
-                    File.WriteAllText(path + "me.rtf", plainText, Encoding.Unicode);
-                    File.WriteAllText(path + "me.txt", plainText, Encoding.Unicode);
+                    //File.WriteAllText(path + "Koçtaş İmza.rtf", plainText, Encoding.Default);
+                    File.WriteAllText(path + "Koçtaş İmza.txt", plainText, Encoding.Unicode);
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("İmza Dosyaları oluşturulurken hata..\n" + ex.Message,"Hata!");
+
+                    File.Create(path + "error.error");
                     Environment.Exit(0);
                 }
 
-                MessageBox.Show("me.htm\nme.rtf\nme.txt\ndosyaları "+path+" dizinine başarıyla oluşturuldu.","Başarılı");
-                Process.Start(path);
-                
+                //MessageBox.Show("me.htm\nme.rtf\nme.txt\ndosyaları "+path+" dizinine başarıyla oluşturuldu.","Başarılı");
+                //Process.Start(path);
+                File.WriteAllText(path + "/success.success", "By Keskin");
                 Environment.Exit(0);
             }
         }
